71333b2d91080caec69276b1bfa641ab0bf36451
Sharma <ssharma@innodata.com>
2025-01-30 13:06:26 +0530

    check changes

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -6,7 +6,7 @@ import { OrderCancelledListener } from './event/listeners/order-cancelled-listen
 
 const start = async () => {
   if (!process.env.JWT_KEY) {
-    throw new Error('JWT_KEY must be defined');
+    throw new Error('JWT_KEY must be defined ....');
   }
   if (!process.env.MONGO_URI) {
     throw new Error('MONGO_URI must be defined');
